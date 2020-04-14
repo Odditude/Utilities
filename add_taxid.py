e1ff62e80a8d8204b8eae8e5f96805dd7263d9f4
Leandro <56632672+Odditude@users.noreply.github.com>
2020-04-14 17:44:24 +0200

    Update add_taxid.py
    Added taxid as an argument

--- add_taxid.py
+++ add_taxid.py
@@ -1,18 +1,19 @@
 import sys
 database = sys.argv[1]
-taxID_database = sys.argv[2]
+wanted_taxid = sys.argv[2]
+taxID_database = sys.argv[3]
 
 with open(database, 'r') as fa_in:
     with open(taxID_database, 'w') as fa_out:
         for line in fa_in:
             if line.startswith(">"):
-                fa_out.write("|kraken:taxid|50557 ".join(line.split(" ", 1)))
+                fa_out.write("|kraken:taxid|wanted_taxid ".join(line.split(" ", 1)))
             else:
                 fa_out.write(line)
                 
 '''
 Uses:
-python add_taxid.py input output
+python add_taxid.py input taxid output
 input is a fasta file
 
 Used to change the fasta header to make it appropriate for kraken
