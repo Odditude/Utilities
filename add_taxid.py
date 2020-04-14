8f7b26a5a220566d50d8ca1b5194c30db8785ab1
Leandro <56632672+Odditude@users.noreply.github.com>
2020-04-14 17:55:59 +0200

    Update add_taxid.py

--- add_taxid.py
+++ add_taxid.py
@@ -7,14 +7,13 @@ with open(database, 'r') as fa_in:
     with open(taxID_database, 'w') as fa_out:
         for line in fa_in:
             if line.startswith(">"):
-                fa_out.write("|kraken:taxid|wanted_taxid ".join(line.split(" ", 1)))
+                fa_out.write(("|kraken:taxid|" + str(wanted_taxid) + " ").join(line.split(" ", 1)))
             else:
                 fa_out.write(line)
-                
+
 '''
 Uses:
 python add_taxid.py input taxid output
 input is a fasta file
-
 Used to change the fasta header to make it appropriate for kraken
 '''
